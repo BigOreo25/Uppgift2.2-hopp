32715a00bde7c6f5fe3da9e11c56ecb909e0c432
BigOreo25 <alts07001@utb.vaxjo.se>
2024-10-09 12:23:46 +0200

    Add project files.

---
+++ Uppgift2.2-hopp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace hoppa
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej Elin! Hur långt hoppade du i meter på längdhoppstävlingen igår?");
+            float längd1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Aaah! Nice. Kommer du ihåg hur långt Alma hoppade också?");
+            float längd2 = float.Parse(Console.ReadLine());
+            float minus = längd1 - längd2;
+            Console.WriteLine("Ok! Så du hoppade " + minus + " meter längre än Alma. bra");
+        }
+
+    }
+
+
+}
